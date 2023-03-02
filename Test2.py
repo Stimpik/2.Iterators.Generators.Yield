22202a1fad3fb5d59a2f023ccfc7980cd7407aec
Igor D <iderik@mail.ru>
2023-03-03 00:06:57 +0300

    Test2

--- Test2.py
+++ Test2.py
@@ -1,25 +1,13 @@
-from copy import deepcopy
+import types
 
 
-class FlatIterator:
+def flat_generator(list_of_lists):
+    for items in list_of_lists:
+        for item in items:
+            yield item
 
-    def __init__(self, list_of_list):
-        self.list_of_list = list_of_list
-        self.list = deepcopy(self.list_of_list)
+def test_2():
 
-    def __iter__(self):
-        self.cursor = 0
-        return self
-
-    def __next__(self):
-        if len(self.list[self.cursor]) == 0:
-            self.cursor += 1
-            if self.cursor == len(self.list):
-                raise StopIteration
-        return self.list[self.cursor].pop(0)
-
-
-def test_1():
     list_of_lists_1 = [
         ['a', 'b', 'c'],
         ['d', 'e', 'f', 'h', False],
@@ -27,13 +15,16 @@ def test_1():
     ]
 
     for flat_iterator_item, check_item in zip(
-            FlatIterator(list_of_lists_1),
+            flat_generator(list_of_lists_1),
             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
     ):
+
         assert flat_iterator_item == check_item
 
-    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
 
 
 if __name__ == '__main__':
-    test_1()
+    test_2()
